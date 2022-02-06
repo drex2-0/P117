2c1ed507b024efb1d8513b8eb0b44405380813eb
drex2-0 <84222894+drex2-0@users.noreply.github.com>
2022-02-06 12:56:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
+quick_draw_data_set=["airpods","apple","alarm clock","raccoon","basketball","magic","bracelet","watch","butterfly","envelope","umbrella","controller"];
+randomnumber=Math.floor((Math.random()*quick_draw_data_set.length)+1);
+sketch=quick_draw_data_set[randomnumber];
+document.getElementById("Sketch_Name").innerHTML="sketch to be drawn: "+sketch;
 function setup()
 {
     canvas=createCanvas(300,300);
     canvas.center();
+    background("white");
+}
+function preload()
+{
+    classifier=ml5.imageClassifier('DoodleNet');
+}
+function clearCanvas()
+{
+    background("white");
+}
+function draw()
+{
+    strokeWeight(13);
+    stroke("black");
+    if(mouseIsPressed)
+    {
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
 }
